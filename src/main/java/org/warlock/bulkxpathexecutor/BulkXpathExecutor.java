9cadcf806703131a752e88278494e6b6683bae72
Simon Farrow <simon.farrow1@nhs.net>
2020-05-26 10:48:44 +0100

    Added TODAY keyword

--- src/main/java/org/warlock/bulkxpathexecutor/BulkXpathExecutor.java
+++ src/main/java/org/warlock/bulkxpathexecutor/BulkXpathExecutor.java
@@ -24,8 +24,15 @@ import java.io.StringReader;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.UUID;
 import javax.xml.namespace.NamespaceContext;
@@ -52,13 +59,19 @@ import org.xml.sax.InputSource;
 public class BulkXpathExecutor {
 
     private static final String USAGE = "Usage: java -jar BulkXpathExecutor.jar -p pathsfile [ -r datafile ] [ -m ] [ -M ] [ -f ] [ -t ] [ -o outputfile ] [ -e errorfile ] [ -x extension ] [ -X extension ] [ documentfile | - ]";
-    private HashMap<String,DescribedXPath> expressions = new HashMap<>();
-    private HashMap<String,ArrayList<String>> substitutions = null;
+    private HashMap<String, DescribedXPath> expressions = new HashMap<>();
+    private HashMap<String, ArrayList<String>> substitutions = null;
     private NamespaceContext nhsdNS = CfHNamespaceContext.getXMLNamespaceContext();
     private static final SimpleDateFormat ISO8601TIME = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
     private static final SimpleDateFormat ISO8601DATE = new SimpleDateFormat("yyyy-MM-dd");
-    
+
     private OutputManager outputManager = null;
+
+    // Reserved words
+    private static final String RESERVED_WORD_UUID = "$UUID";
+    private static final String RESERVED_WORD_TODAY = "$TODAY";
+    private static final String RESERVED_WORD_DATE = "$DATE";
+    private static final String RESERVED_WORD_TIME = "$TIME";
 
     /**
      * @param args the command line arguments
@@ -85,42 +98,41 @@ public class BulkXpathExecutor {
                     ++i;
                     om.setOutputFile(args[i]);
                     continue;
-                }            
+                }
                 if (args[i].contentEquals("-e")) {
                     ++i;
                     om.setErrorFile(args[i]);
                     continue;
-                }            
+                }
                 if (args[i].contentEquals("-x")) {
                     ++i;
                     om.setOutputExtension(args[i]);
                     continue;
-                }            
+                }
                 if (args[i].contentEquals("-X")) {
                     ++i;
                     om.setErrorExtension(args[i]);
                     continue;
-                }            
+                }
                 if (args[i].contentEquals("-m")) {
                     om.setInMemoryOutput();
                     continue;
-                }            
+                }
                 if (args[i].contentEquals("-M")) {
                     om.setInMemoryError();
                     continue;
-                }            
+                }
                 if (args[i].contentEquals("-f")) {
                     om.setPrependFilenameToError(true);
                     continue;
-                }            
+                }
                 if (args[i].contentEquals("-t")) {
                     om.setTimestampError(true);
                     continue;
-                }            
+                }
                 doc.add(args[i]);
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         if (paths == null) {
@@ -150,20 +162,18 @@ public class BulkXpathExecutor {
                     System.err.println(s);
                 }
             }
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
+
     /**
-     * 
+     *
      * @param paths
-     * @throws Exception 
+     * @throws Exception
      */
-    private BulkXpathExecutor(String paths) 
-            throws Exception
-    {
+    private BulkXpathExecutor(String paths)
+            throws Exception {
         @SuppressWarnings("UnusedAssignment")
         InputStream in = null;
         if (paths.contentEquals("-")) {
@@ -176,52 +186,57 @@ public class BulkXpathExecutor {
         String line = null;
         XPathFactory xpf = XPathFactory.newInstance();
         while ((line = br.readLine()) != null) {
-            if (line.contentEquals("."))
+            if (line.contentEquals(".")) {
                 break;
+            }
             if (line.contains("\t")) {
                 String[] s = line.split("\t");
                 XPath xp = xpf.newXPath();
                 xp.setNamespaceContext(nhsdNS);
                 XPathExpression exp = xp.compile(s[1]);
                 DescribedXPath x = new DescribedXPath(s[1], exp);
-                expressions.put(s[0], x);                
+                expressions.put(s[0], x);
             } else {
                 XPath xp = xpf.newXPath();
                 xp.setNamespaceContext(nhsdNS);
                 XPathExpression exp = xp.compile(line);
                 DescribedXPath x = new DescribedXPath(line, exp);
-                expressions.put(line, x);                
+                expressions.put(line, x);
             }
         }
     }
-    
-    private void setOutputManager(OutputManager om) { outputManager = om; }
 
-    private OutputManager getOutputManager() { return outputManager; }
-    
+    private void setOutputManager(OutputManager om) {
+        outputManager = om;
+    }
+
+    private OutputManager getOutputManager() {
+        return outputManager;
+    }
+
     private void processDocuments(ArrayList<String> d)
-            throws Exception
-    {
-        if (outputManager == null)
+            throws Exception {
+        if (outputManager == null) {
             outputManager = new OutputManager();
-        
+        }
+
         for (String s : d) {
             outputManager.setCurrentFile(s);
             process(s);
         }
     }
-      
+
     /**
      *
      * @param d
      * @throws Exception
      */
     private void setData(String d)
-            throws Exception
-    {
-        if (d == null)
+            throws Exception {
+        if (d == null) {
             return;
-        
+        }
+
         substitutions = new HashMap<>();
         BufferedReader br = new BufferedReader(new FileReader(d));
         @SuppressWarnings("UnusedAssignment")
@@ -242,58 +257,82 @@ public class BulkXpathExecutor {
             substitutions.put(s[0], list);
         }
     }
-    
+
     private String resolveFunction(String s) {
-        
+
         // TODO: Add ISO8601 duration offsets to $TIME and $DATE
-        if (s.contentEquals("$TIME")) {
+        if (s.contentEquals(RESERVED_WORD_TIME)) {
             return ISO8601TIME.format(new Date());
         }
-        if (s.contentEquals("$DATE")) {
+        if (s.contentEquals(RESERVED_WORD_DATE)) {
             return ISO8601DATE.format(new Date());
         }
-        
-        if (s.startsWith("$TIME")) {
+        // $TODAY is a timestamp starting at 00:00:00 on morning of the day in question
+        if (s.contentEquals(RESERVED_WORD_TODAY)) {
+            Calendar cal = new GregorianCalendar();
+            cal.set(Calendar.HOUR_OF_DAY, 0);
+            cal.set(Calendar.MINUTE, 0);
+            cal.set(Calendar.SECOND, 0);
+            cal.set(Calendar.MILLISECOND, 0);
+            return ISO8601TIME.format(cal.getTime());
+        }
+
+        if (s.startsWith(RESERVED_WORD_TIME)) {
             return resolveOffset(s, false);
         }
-        if (s.startsWith("$DATE")) {
+        if (s.startsWith(RESERVED_WORD_DATE)) {
             return resolveOffset(s, true);
         }
-        if (s.startsWith("$UUID")) {
+        if (s.startsWith(RESERVED_WORD_TODAY)) {
+            return resolveOffset(s, false);
+        }
+        if (s.startsWith(RESERVED_WORD_UUID)) {
             return UUID.randomUUID().toString().toLowerCase();
         }
         // Reference to something else, which we'll resolve as we need it
         //
         return s;
     }
-    
+
     private String resolveOffset(String spec, boolean dt) {
-        
-        String d = spec.substring("$TIME".length()).trim();
-        
+
+        String d = null;
+        Instant now = null;
+        if (spec.startsWith(RESERVED_WORD_TODAY)) {
+            d = spec.substring(RESERVED_WORD_TODAY.length()).trim();
+            LocalTime midnight = LocalTime.MIDNIGHT;
+            LocalDate today = LocalDate.now(ZoneId.of("Europe/London"));
+            LocalDateTime todayMidnight = LocalDateTime.of(today, midnight);
+            // TODO I don't understand why this appears to work during BST
+            now = todayMidnight.toInstant(ZoneOffset.UTC);
+        } else {
+            d = spec.substring(RESERVED_WORD_TIME.length()).trim();
+            now = Instant.now();
+        }
+
         Duration offset = Duration.parse(d);
-        Instant now = Instant.now();
+
         @SuppressWarnings("UnusedAssignment")
         Instant then = null;
         then = now.plus(offset);
         // Should be ISO-8601 anyway
-        if(!dt)
-            return then.toString();
+        if (!dt) {
+            return then.toString().substring(0, 19);
+        }
         String t = then.toString();
         return t.substring(0, t.indexOf("T"));
     }
-        
-    private void process(String doc) 
-            throws Exception
-    {
+
+    private void process(String doc)
+            throws Exception {
         boolean streamOutput = true;
-        
+
         @SuppressWarnings("UnusedAssignment")
         Document d = getDocument(doc);
-        for(String s : expressions.keySet()) {
+        for (String s : expressions.keySet()) {
             DescribedXPath xp = expressions.get(s);
             XPathExpression exp = xp.getExpression();
-            NodeList nl = (NodeList)exp.evaluate(d, XPathConstants.NODESET);
+            NodeList nl = (NodeList) exp.evaluate(d, XPathConstants.NODESET);
             if (substitutions == null) {
                 StringBuilder sb = new StringBuilder(s);
                 sb.append(System.getProperty("line.separator"));
@@ -325,10 +364,10 @@ public class BulkXpathExecutor {
                         if (v.startsWith("xmlfragment:")) {
                             if (n.getNodeType() == Node.ELEMENT_NODE) {
                                 Element elem = getElement(v.substring("xmlfragment:".length()));
-                                elem = (Element)d.importNode(elem, true);
+                                elem = (Element) d.importNode(elem, true);
                                 n.getParentNode().replaceChild(elem, n);
                             } else {
-                                StringBuilder erep = new StringBuilder("WARNING: Ignoring substitution. Attempt to substitute XML fragment "); 
+                                StringBuilder erep = new StringBuilder("WARNING: Ignoring substitution. Attempt to substitute XML fragment ");
                                 erep.append(v.substring("xmlfragment:".length()));
                                 erep.append(" into non-element location ");
                                 erep.append(xp.getXpath());
@@ -352,10 +391,9 @@ public class BulkXpathExecutor {
                                 }
                             } else {
                                 n.setNodeValue(v);
-                            }                            
+                            }
                         }
-                    }
-                    catch (IndexOutOfBoundsException e) {
+                    } catch (IndexOutOfBoundsException e) {
                         n.setNodeValue("");
                     }
                 }
@@ -365,36 +403,33 @@ public class BulkXpathExecutor {
             outputManager.output(getStringFromDoc(d));
         }
     }
-    
+
     private Element getElement(String s)
-            throws Exception
-    {
+            throws Exception {
         Element elem = parse(s).getDocumentElement();
         return elem;
     }
-    
-    private String getStringFromDoc(Document doc)    {
+
+    private String getStringFromDoc(Document doc) {
         DOMImplementationLS domImplementation = (DOMImplementationLS) doc.getImplementation();
         LSSerializer lsSerializer = domImplementation.createLSSerializer();
         DOMStringList x = lsSerializer.getDomConfig().getParameterNames();
-        return lsSerializer.writeToString(doc);   
-    }    
+        return lsSerializer.writeToString(doc);
+    }
 
     private Document parse(String s)
-            throws Exception
-    {
+            throws Exception {
         InputSource is = new InputSource(new StringReader(s));
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         dbf.setIgnoringComments(true);
         dbf.setNamespaceAware(true);
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document xml = db.parse(is);
-        return xml;        
+        return xml;
     }
-    
+
     private Document getDocument(String d)
-            throws Exception
-    {
+            throws Exception {
         StringBuilder sb = new StringBuilder();
         @SuppressWarnings("UnusedAssignment")
         InputStream in = null;
